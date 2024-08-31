34aca59a6522eefe30120a645ab9de3c545b7e51
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2024-09-01 00:14:48 +0900

    a

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # fluttertest
 
-https://codelabs.developers.google.com/codelabs/flutter-codelab-first?hl=ja
+![build](https://github.com/Akatsuki1910/fluttertest/actions/workflows/builld.yml/badge.svg)
+
+<https://codelabs.developers.google.com/codelabs/flutter-codelab-first?hl=ja>
+
+```sh
+flutter run -d chrome
+```
--- lib/test/start_page.dart
+++ lib/test/start_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/link.dart';
 
 final uri = Uri.parse('https://github.com/Akatsuki1910/fluttertest');
+final webPage = Uri.parse('https://akatsuki1910.github.io/fluttertest/');
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
@@ -9,16 +11,35 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Link(
-        uri: uri,
-        target: LinkTarget.blank,
-        builder: (context, followLink) {
-          return MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: ElevatedButton(
-                  onPressed: followLink, child: const Text('GitHub')));
-        },
-      ),
-    );
+        child: Wrap(
+      spacing: 10,
+      direction: Axis.vertical,
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        Link(
+          uri: uri,
+          target: LinkTarget.blank,
+          builder: (context, followLink) {
+            return MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: ElevatedButton(
+                    onPressed: followLink, child: const Text('GitHub')));
+          },
+        ),
+        Link(
+          uri: webPage,
+          target: LinkTarget.blank,
+          builder: (context, followLink) {
+            return MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: ElevatedButton(
+                    onPressed: followLink, child: const Text('Web Page')));
+          },
+        ),
+        SvgPicture.network(
+          'https://github.com/Akatsuki1910/fluttertest/actions/workflows/build.yml/badge.svg',
+        ),
+      ],
+    ));
   }
 }
